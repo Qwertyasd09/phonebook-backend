acdb496fe47f6085f8d0ebe1bcbce7c5fe2027c7
Diego Armando Chiri Torres <dgchiri@gmail.com>
2024-06-04 01:44:27 -0400

    exercises 3.15-3.18 done

--- index.js
+++ index.js
@@ -1,106 +1,86 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3001
-const morgan = require("morgan")
-const cors = require("cors")
-
-app.use(express.json())
-app.use(cors())
-app.use(morgan(function (tokens, req, res) {
-  return [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens.res(req, res, 'content-length'), '-',
-    tokens['response-time'](req, res), 'ms',
-    JSON.stringify(req.body)
-  ].join(' ')
-}))
+require('dotenv').config()
 app.use(express.static('dist'))
+app.use(express.json())
+const PORT = process.env.PORT
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+const mongoose = require('mongoose')
+const PersonPhonebook = require('./models/person')
+const url = process.env.MONGODB_URI
 
-const getCurrentDate = () => {
-  return new Date
-}
-
-const generateId = () => {
-  return Math.floor((Math.random() * 1e6) + 1);
-}
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
-})
-
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+  PersonPhonebook.find({}).then(persons => {
+    res.json(persons)
+  })
 })
 
 app.post('/api/persons', (req, res) => {
-  
   const body = req.body
-
   if (!body.name || !body.number) {
     return res.status(400).json({
       error: "name or number missing"
     })
   }
-
-  if (persons.map(person => person.name).includes(body.name)) {
-    return res.status(400).json({
-      error: "name must be unique"
-    })
-  }
-
-  const person = {
-    id: generateId(),
+  const person = new PersonPhonebook ({
     name: body.name,
     number: body.number 
-  }
-  persons = persons.concat(person)
-  res.json(person)
+  })
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-  res.status(204).end()
+app.put('/api/persons/:id', (req, res)  => {
+  PersonPhonebook.findOneAndUpdate(
+    { name: req.body.name }, 
+    { number: req.body.number }, 
+    { new: true })
+    .then(updatedPerson => res.json(updatedPerson))
+    .catch(error => next(error))
 })
 
-app.get('/info', (req, res) => {
-  const numOfPersons = persons.length
-  res.send(`
-  <p>Phonebook has info for ${numOfPersons} people</p>
-  <p>${getCurrentDate()}</p>
-  `)
+app.get('/api/persons/:id', (req, res, next) => {
+  PersonPhonebook.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
+
+app.delete('/api/persons/:id', (req, res, next) => {
+  PersonPhonebook.findByIdAndDelete(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
